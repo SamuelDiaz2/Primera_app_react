270a2bf9917dda90fb88cf8a9c949d4f397379ea
samueldiaz2 <samuel.diazf@uniagustiniana.edu.co>
2025-08-20 15:51:19 +0000

    presentacion

--- src/components/saludo.js
+++ src/components/saludo.js
@@ -2,10 +2,23 @@ function Saludo() {
   return <h1>Hola desde mi primer componente</h1>;
 }
 
+function Presentacion() {
+  return (
+    <p>
+      <strong>Mi nombre es:</strong> Samuel Diaz
+      <br></br>
+      <strong>Mi Meta:</strong> Aprendar lo que mas pueda de react
+      <br></br>
+      <strong>Mi comida favorita:</strong> Bandeja paisa
+    </p>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <Saludo />
+      <Presentacion />
     </div>
   );
 }
